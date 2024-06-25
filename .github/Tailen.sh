1d98b94d9d926d5a97214c0f8fe5376b35da3969
Chấm <chamchamvtg@gmail.com>
2024-06-25 14:32:14 +0700

    test

---
+++ .github/Tailen.sh
@@ -0,0 +1,9 @@
+. .github/Function.sh
+cd $TOME/.github/libpy/Flash2in1
+if [ -e $TOME/ok ]; then
+zip -qr $TOME/$NEMEROM *
+fi
+echo "HOST="frs.sourceforge.net"
+USERNAME="chamchamfy"
+PASSWORD="truonggiang910"
+PORT="22"" >> $GITHUB_ENV
