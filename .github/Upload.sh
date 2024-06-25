470fc6b825666f166e11eaf0608821a5fc8454f8
Chấm <111698740+chamchamfy@users.noreply.github.com>
2024-06-25 20:39:23 +0700

    U

--- .github/Upload.sh
+++ .github/Upload.sh
@@ -17,10 +17,14 @@ url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
 eval "curl -F 'file=@$TOME/$NEMEROM' 'https://$url2.gofile.io/uploadFile' > $TOME/1.json"
 LINKROM="$(cat $TOME/1.json | jq -r .data.downloadPage)"
 fi
+Chatbot ' - Tải ROM lên sourceforge.net...'
+tailensf() { TTK='rrom'; TND=$(echo 'Y2hhbWNoYW1meQ==' | base64 -d); MK=$(echo 'dHJ1b25nZ2lhbmc5MTA=' | base64 -d); curl -1 -v -k "sftp://frs.sourceforge.net:/home/frs/project/$TTK/$NEMEROM" --user "$TND:$MK" -T "$TOME/$NEMEROM"; }
+tailensf
 
 # Link download 
 echo
 echo "Link download: $LINKROM"
+echo "Link download (sourceforge.net): https://sourceforge.net/projects/$TTK/files/$NEMEROM"
 
 closechat "Tạo rom thành công <br/><br/>Link Download: $LINKROM"
 addlabel "Hoàn thành"
