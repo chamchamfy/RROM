21d00689948e26246c8c75349e48b821a5159eda
Chấm <111698740+chamchamfy@users.noreply.github.com>
2024-08-13 08:25:42 +0700

    Xóa workflow run

--- .github/Test.sh
+++ .github/Test.sh
@@ -49,8 +49,22 @@ else echo "- Không có tập tin rom"
 fi 
 }
 
-Taidulieu
-ls $TOME/$Tenrom
-ls $TOME/Unzip
+xoaw() {
+repo=RROM
+user=chamchamfy
+WORKFLOW_RUN_IDS=$(gh api /repos/${user}/${repo}/actions/runs | jq -r ".workflow_runs[] | [.id] | @tsv")
+
+for id in ${WORKFLOW_RUN_IDS}
+do
+    echo "Workflow ID: ${id}"
+    gh api -X DELETE "/repos/${user}/${repo}/actions/runs/${id}"
+done
+}
+
+#Taidulieu
+#ls $TOME/$Tenrom
+#ls $TOME/Unzip
 #. $TOME/Option.md
+# Xóa workflow_run
+xoaw
 echo "- Kết thúc" 
