beec632fae89d87a570990969613063a6c6d50a4
Chấm <111698740+chamchamfy@users.noreply.github.com>
2025-04-12 13:30:04 +0700

    Update Upload.sh

--- .github/Upload.sh
+++ .github/Upload.sh
@@ -18,7 +18,7 @@ LINKROMSFG="https://sourceforge.net/projects/$TTK/files/$NEMEROM"
 }
 svpx() {
 APIK='fc200943-6990-403d-b187-dcf57dfb7526'
-eval "curl -T '$TOME/$NEMEROM' -u :'$APIK' 'https://pixeldrain.com/api/file/' > $TOME/1.json"
+eval "curl -T '$TOME/$NEMEROM' --connect-timeout 20 -u :'$APIK' 'https://pixeldrain.com/api/file/' > $TOME/1.json"
 #curl -1 -v -k "https://pixeldrain.com/api/user/files" --user "*:*" -T "$TOME/$NEMEROM" 
 LINKROMPX="https://pixeldrain.com/u/$(cat "$TOME/1.json" | jq -r .id)"
 }
