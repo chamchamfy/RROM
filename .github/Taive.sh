0ab5a7480219e142dc348e55c6fbf20896176ab2
Chấm <111698740+chamchamfy@users.noreply.github.com>
2025-11-05 09:41:09 +0700

    c

--- .github/Taive.sh
+++ .github/Taive.sh
@@ -92,18 +92,9 @@ done
 ) 
 Chatbot "- Giải nén ROM ${URL##*/} ..."
 if [ -e "$TOME/rom.x" ]; then
- if [ -n "$(xxd -l 4 -c 4 $TOME/rom.x | grep '504b')" ]; then
- mv -f "$TOME/rom.x" "$TOME/rom.zip"
- unzip -qo "$TOME/rom.zip" -d "$TOME/Unzip" 2>/dev/null
+ unzip -qo "$TOME/rom.x" -d "$TOME/Unzip" 2>/dev/null || tar -xf "$TOME/rom.x" -C "$TOME/Unzip"
+ [ $? -ne 0 ] && bug "- Rom không phải file zip hoặc tgz, gz"
  cp -rf $TOME/Unzip/META-INF/com/android $TOME/.github/libpy/Flash2in1/META-INF/com 2>/dev/null
- elif [ -n "$(xxd -l 4 -c 4 $TOME/rom.x | grep '1f8b 0808')" ]; then 
- mv -f "$TOME/rom.x" "$TOME/rom.gz"
- tar -xf "$TOME/rom.gz" -C "$TOME/Unzip"
- elif [ -n "$(xxd -l 4 -c 4 $TOME/rom.x | grep '1f8b 0880')" ]; then 
- mv -f "$TOME/rom.x" "$TOME/rom.tgz"
- tar -xf "$TOME/rom.tgz" -C "$TOME/Unzip"
- else bug "- Rom không phải file zip hoặc tgz, gz"
- fi
  NEMEROM="RROM_${DDPV}_${URL##*/}.zip"
  echo "NEMEROM=$NEMEROM" >> $GITHUB_ENV
 fi
