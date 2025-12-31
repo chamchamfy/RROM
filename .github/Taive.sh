6b5f099ab23146745d8369747cf0e6cbe11f6163
Chấm <111698740+chamchamfy@users.noreply.github.com>
2025-12-31 14:33:54 +0700

    b

--- .github/Taive.sh
+++ .github/Taive.sh
@@ -70,7 +70,7 @@ if [ "$URL" ]; then
 sudo apt-get update >/dev/null
 sudo apt-get install zstd binutils e2fsprogs erofs-utils simg2img img2simg zipalign f2fs-tools p7zip >/dev/null
 pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome >/dev/null
-#echo "protobuf<=3.20.1" > requirements.txt
+echo "protobuf<=3.20.1" > requirements.txt
 pip3 install -r requirements.txt >/dev/null;
 ) & ( 
 Chatbot "- Bắt đầu tải ROM: $URL ...";
