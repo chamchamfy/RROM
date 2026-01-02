f92a4af18ca257469cf5f228ae1eb05b2be49807
Chấm <111698740+chamchamfy@users.noreply.github.com>
2026-01-02 14:27:27 +0700

    M

--- .github/Taive.sh
+++ .github/Taive.sh
@@ -71,10 +71,10 @@ if [ "$URL" ]; then
 sudo apt-get update >/dev/null
 sudo apt-get install zstd binutils e2fsprogs erofs-utils simg2img img2simg zipalign f2fs-tools p7zip xz-utils >/dev/null
 #
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update >/dev/null
-sudo apt-get install python3.12 python3.12-venv python3.12-dev
+PYM=$(apt-cache search ^python3.[0-9]+$ | awk '{print $1}' | sort -V | tail -n1)
+sudo apt-get install -y $PYM ${PYM}-venv ${PYM}-dev python3-pip
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/$PYM 2
+sudo update-alternatives --config python3
 #
 python3 -m pip install --upgrade pip
 pip3 install protobuf bsdiff4 six brotli crypto construct google docopt pycryptodome zstandard 
