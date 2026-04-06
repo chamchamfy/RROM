65553c8b74f5823c8f10dbc1deeeb8cd466b2c6c
Chấm <111698740+chamchamfy@users.noreply.github.com>
2026-04-06 16:00:31 +0700

    UP Function.sh

--- .github/Function.sh
+++ .github/Function.sh
@@ -6,6 +6,7 @@ mkdir -p $TOME/{tmp,Unpack,Repack,Unzip,Payload,Super,Apk,Mod/tmp,VH,Up}
 
 Xem() { curl -sLGN -H "$User" --connect-timeout 20 "$1"; }
 Taive() { curl -sLk -H "$User" --connect-timeout 20 "$1" -o "$2"; }
+TAI() { aria2c -c -x8 -s8 -d $3 -o "$2" "$1"; }
 Taiver() { curl -Sk "$1" -o "$2"; }
 Taivewget() { wget "$1" -O "$2"; }
 Taivewgetr() { wget --no-check-certificate "$1" -O "$2"; }
