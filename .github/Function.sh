d4476f03c2e200ccb19375d9cc7a5d4077f7db2a
Chấm <111698740+chamchamfy@users.noreply.github.com>
2026-04-06 15:56:02 +0700

    up

--- .github/Function.sh
+++ .github/Function.sh
@@ -1,6 +1,6 @@
 # Function
 export TOME="$GITHUB_WORKSPACE"
-export User="User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36"
+export User="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
 
 mkdir -p $TOME/{tmp,Unpack,Repack,Unzip,Payload,Super,Apk,Mod/tmp,VH,Up}
 
